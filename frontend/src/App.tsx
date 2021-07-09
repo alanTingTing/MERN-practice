801093727030a8ce78561dab913c94ecb651e48c
alanTingTing <alan.yhting@gmail.com>
2021-07-09 08:45:22 +0000

    done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import TodoItem from './components/TodoItem'
 import AddTodo from './components/AddTodo'
-import { getTodos, addTodo, updateTodo, deleteTodo } from './API'
+import { getTodos,  addTodo, updateTodo, deleteTodo} from './API'
 import './App.css'
 
 const App = () => {
